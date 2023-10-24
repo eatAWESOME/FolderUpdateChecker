9bb96d80b72a42a259be11bf4c51427a3ef6f9af
Alden Tilley <atilley12@gmail.com>
2023-10-23 21:23:26 -0400

    Added options to change check interval, alert type, and wake interval

--- Folder Update Checker.py
+++ Folder Update Checker.py
@@ -1,7 +1,6 @@
 '''
 Author: Alden Tilley
 https://github.com/eatAWESOME
-Date: 2/23/2023
 '''
 
 import os
@@ -12,7 +11,7 @@ import winsound
 import pyautogui
 import threading
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 
 def Start():
     StartButton["state"] = tk.DISABLED
@@ -83,6 +82,7 @@ def CheckFolders():
         
         
     i = 0
+    w = 0
     while StopButton["state"] != tk.DISABLED:
         if i == 0:
             if os.path.exists(Folder1Var.get()):
@@ -147,12 +147,17 @@ def CheckFolders():
                 Text1_1["text"] = str(t.hour) + ":" + "%02d" % t.minute
                 if os.path.exists(Folder1Var.get()):
                     Text1_2["text"] = "Folder 1 Changed"
+                    if AlertVar.get() == "Beep on Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text1_2["text"] = "Folder 1 Not Found"
-                winsound.Beep(200, 1000)
+                    winsound.Beep(200, 1000)
             else:
-                if os.path.exists(Folder1Var.get()) or Folder1Var.get() == "":
+                if Folder1Var.get() == "":
                     Text1_2["text"] = ""
+                elif os.path.exists(Folder1Var.get()):
+                    if AlertVar.get() == "Beep on No Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text1_2["text"] = "Folder 1 Not Found"
                     winsound.Beep(200, 1000)
@@ -163,16 +168,21 @@ def CheckFolders():
                 AT2 = BT2
                 t = datetime.datetime.now()
                 Text2_1["text"] = str(t.hour) + ":" + "%02d" % t.minute
-                if os.path.exists(Folder2Var.get()):
+                if os.path.exists(Folder1Var.get()):
                     Text2_2["text"] = "Folder 2 Changed"
+                    if AlertVar.get() == "Beep on Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text2_2["text"] = "Folder 2 Not Found"
-                winsound.Beep(200, 1000)
+                    winsound.Beep(200, 1000)
             else:
-                if os.path.exists(Folder2Var.get()) or Folder2Var.get() == "":
+                if Folder2Var.get() == "":
                     Text2_2["text"] = ""
+                elif os.path.exists(Folder2Var.get()):
+                    if AlertVar.get() == "Beep on No Change":
+                        winsound.Beep(200, 1000)
                 else:
-                    Text2_2["text"] = "Folder 2 Not Found"
+                    Text2_2["text"] = "Folder 1 Not Found"
                     winsound.Beep(200, 1000)
                 
                 
@@ -183,12 +193,17 @@ def CheckFolders():
                 Text3_1["text"] = str(t.hour) + ":" + "%02d" % t.minute
                 if os.path.exists(Folder3Var.get()):
                     Text3_2["text"] = "Folder 3 Changed"
+                    if AlertVar.get() == "Beep on Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text3_2["text"] = "Folder 3 Not Found"
-                winsound.Beep(200, 1000)
+                    winsound.Beep(200, 1000)
             else:
-                if os.path.exists(Folder3Var.get()) or Folder3Var.get() == "":
+                if Folder3Var.get() == "":
                     Text3_2["text"] = ""
+                elif os.path.exists(Folder3Var.get()):
+                    if AlertVar.get() == "Beep on No Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text3_2["text"] = "Folder 3 Not Found"
                     winsound.Beep(200, 1000)
@@ -201,12 +216,17 @@ def CheckFolders():
                 Text4_1["text"] = str(t.hour) + ":" + "%02d" % t.minute
                 if os.path.exists(Folder4Var.get()):
                     Text4_2["text"] = "Folder 4 Changed"
+                    if AlertVar.get() == "Beep on Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text4_2["text"] = "Folder 4 Not Found"
-                winsound.Beep(200, 1000)
+                    winsound.Beep(200, 1000)
             else:
-                if os.path.exists(Folder4Var.get()) or Folder4Var.get() == "":
+                if Folder4Var.get() == "":
                     Text4_2["text"] = ""
+                elif os.path.exists(Folder4Var.get()):
+                    if AlertVar.get() == "Beep on No Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text4_2["text"] = "Folder 4 Not Found"
                     winsound.Beep(200, 1000)
@@ -219,25 +239,64 @@ def CheckFolders():
                 Text5_1["text"] = str(t.hour) + ":" + "%02d" % t.minute
                 if os.path.exists(Folder5Var.get()):
                     Text5_2["text"] = "Folder 5 Changed"
+                    if AlertVar.get() == "Beep on Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text5_2["text"] = "Folder 5 Not Found"
-                winsound.Beep(200, 1000)
+                    winsound.Beep(200, 1000)
             else:
-                if os.path.exists(Folder5Var.get()) or Folder5Var.get() == "":
+                if Folder5Var.get() == "":
                     Text5_2["text"] = ""
+                elif os.path.exists(Folder5Var.get()):
+                    if AlertVar.get() == "Beep on No Change":
+                        winsound.Beep(200, 1000)
                 else:
                     Text5_2["text"] = "Folder 5 Not Found"
                     winsound.Beep(200, 1000)
                 
+        
+        try:
+            CheckInterval = int(CheckIntervalVar.get())
+            if CheckIntervalUnitsVar.get() == "minutes":
+                CheckInterval *= 60
+            elif CheckIntervalUnitsVar.get() == "hours":
+                CheckInterval *= 60 * 60
+            if CheckInterval < 1:
+                CheckInterval = 1
+        except:
+            CheckInterval = 60
+            if CheckIntervalVar.get() != "":
+                CheckIntervalVar.delete(0, tk.END)
+                CheckIntervalVar.insert(0, "60")
+        
+        i += 1
+        if i == CheckInterval:
+            i = 0
+            
+            
+        if Wake.get():
+            try:
+                WakeInterval = int(WakeIntervalVar.get())
+                if WakeIntervalUnitsVar.get() == "minutes":
+                    WakeInterval *= 60
+                elif WakeIntervalUnitsVar.get() == "hours":
+                    WakeInterval *= 60 * 60
+                if WakeInterval < 1:
+                    WakeInterval = 1
+            except:
+                WakeInterval = 60
+                if WakeIntervalVar.get() != "":
+                    WakeIntervalVar.delete(0, tk.END)
+                    WakeIntervalVar.insert(0, "60")
                 
-        if i == 60:
-            if Wake.get():
+            w += 1
+            if w == WakeInterval:
                 pyautogui.press('volumemute')
                 time.sleep(0.1)
                 pyautogui.press('volumemute')
-            i = 0
-        else:
-            i += 1
+                w = 0
+        
+        
         time.sleep(1)
 
 def StopThread():
@@ -292,72 +351,108 @@ def Folder5Browser():
     Folder5Var.delete(0, tk.END)
     Folder5Var.insert(0, root.filename)
 
+def WakeIntervalToggle():
+    if Wake.get():
+        WakeIntervalText.grid(row = 24, column = 2, columnspan = 15, sticky = tk.E)
+        WakeIntervalVar.grid(row = 24, column = 17, sticky = tk.W)
+        WakeIntervalUnitsBox.grid(row = 24, column = 18, columnspan = 14, sticky = tk.W)
+    else:
+        WakeIntervalText.grid_forget()
+        WakeIntervalVar.grid_forget()
+        WakeIntervalUnitsBox.grid_forget()
+
 if __name__ == "__main__":
     
     root = tk.Tk()
     root.title("Folder Update Checker")
     
-    Title = tk.Label(root, text = "Folder Update Checker").grid(row = 0, column = 0, columnspan = 9)
+    Title = tk.Label(root, text = "Folder Update Checker").grid(row = 0, column = 0, columnspan = 34)
     
     Spacer1 = tk.Label(root, text = "     ").grid(row = 10, column = 0)
-    Spacer2 = tk.Label(root, text = "     ").grid(row = 20, column = 10)
+    Spacer2 = tk.Label(root, text = "     ").grid(row = 20, column = 33)
     Spacer3 = tk.Label(root, text = "     ").grid(row = 30, column = 0)
     Spacer4 = tk.Label(root, text = "     ").grid(row = 40, column = 0)
+    Spacer4 = tk.Label(root, text = "     ").grid(row = 50, column = 0)
     
     Folder1Text = tk.Label(root, text = "Folder 1:").grid(row = 11, column = 1)
     Folder1Var = tk.Entry(root, width = 90)
-    Folder1Var.grid(row = 11, column = 2, columnspan = 5)
-    Folder1Button = tk.Button(root, text = "Browse", command = Folder1Browser).grid(row = 11, column = 7)
+    Folder1Var.grid(row = 11, column = 2, columnspan = 30)
+    Folder1Button = tk.Button(root, text = "Browse", command = Folder1Browser).grid(row = 11, column = 32)
     
     Folder2Text = tk.Label(root, text = "Folder 2:").grid(row = 12, column = 1)
     Folder2Var = tk.Entry(root, width = 90)
-    Folder2Var.grid(row = 12, column = 2, columnspan = 5)
-    Folder2Button = tk.Button(root, text = "Browse", command = Folder2Browser).grid(row = 12, column = 7)
+    Folder2Var.grid(row = 12, column = 2, columnspan = 30)
+    Folder2Button = tk.Button(root, text = "Browse", command = Folder2Browser).grid(row = 12, column = 32)
     
     Folder3Text = tk.Label(root, text = "Folder 3:").grid(row = 13, column = 1)
     Folder3Var = tk.Entry(root, width = 90)
-    Folder3Var.grid(row = 13, column = 2, columnspan = 5)
-    Folder3Button = tk.Button(root, text = "Browse", command = Folder3Browser).grid(row = 13, column = 7)
+    Folder3Var.grid(row = 13, column = 2, columnspan = 30)
+    Folder3Button = tk.Button(root, text = "Browse", command = Folder3Browser).grid(row = 13, column = 32)
     
     Folder4Text = tk.Label(root, text = "Folder 4:").grid(row = 14, column = 1)
     Folder4Var = tk.Entry(root, width = 90)
-    Folder4Var.grid(row = 14, column = 2, columnspan = 5)
-    Folder4Button = tk.Button(root, text = "Browse", command = Folder4Browser).grid(row = 14, column = 7)
+    Folder4Var.grid(row = 14, column = 2, columnspan = 30)
+    Folder4Button = tk.Button(root, text = "Browse", command = Folder4Browser).grid(row = 14, column = 32)
     
     Folder5Text = tk.Label(root, text = "Folder 5:").grid(row = 15, column = 1)
     Folder5Var = tk.Entry(root, width = 90)
-    Folder5Var.grid(row = 15, column = 2, columnspan = 5)
-    Folder5Button = tk.Button(root, text = "Browse", command = Folder5Browser).grid(row = 15, column = 7)
+    Folder5Var.grid(row = 15, column = 2, columnspan = 30)
+    Folder5Button = tk.Button(root, text = "Browse", command = Folder5Browser).grid(row = 15, column = 32)
     
+    CheckIntervalText = tk.Label(root, text = "Check Interval:").grid(row = 21, column = 2, columnspan = 15, sticky = tk.E)
+    CheckIntervalVar = tk.Entry(root, width = 3, justify = "center")
+    CheckIntervalVar.insert(0, "60")
+    CheckIntervalVar.grid(row = 21, column = 17, sticky = tk.W)
+    CheckIntervalUnitsVar = tk.StringVar()
+    CheckIntervalUnitsBox = ttk.Combobox(root, width = 7, textvariable = CheckIntervalUnitsVar, values = ["seconds", "minutes", "hours"])
+    CheckIntervalUnitsBox.current(0)
+    CheckIntervalUnitsBox.grid(row = 21, column = 18, columnspan = 14, sticky = tk.W)
+    
+    AlertText = tk.Label(root, text = "Alert:").grid(row = 22, column = 2, columnspan = 15, sticky = tk.E)
+    AlertVar = tk.StringVar()
+    AlertBox = ttk.Combobox(root, width = 18, textvariable = AlertVar, values = ["Beep on Change", "Beep on No Change", "No Alert"])
+    AlertBox.current(0)
+    AlertBox.grid(row = 22, column = 17, columnspan = 15, sticky = tk.W)
+    
+    WakeText = tk.Label(root, text = "Keep Awake:").grid(row = 23, column = 2, columnspan = 15, sticky = tk.E)
     Wake = tk.BooleanVar()
-    WakeBox = tk.Checkbutton(root, text = "Keep Awake", var = Wake, onvalue = True, offvalue = False)
+    WakeBox = tk.Checkbutton(root, var = Wake, onvalue = True, offvalue = False, command = WakeIntervalToggle)
+    WakeBox.grid(row = 23, column = 17, columnspan = 15, sticky = tk.W)
+    
+    WakeIntervalText = tk.Label(root, text = "Wake Interval:")
+    WakeIntervalVar = tk.Entry(root, width = 3, justify = "center")
+    WakeIntervalVar.insert(0, "60")
+    WakeIntervalUnitsVar = tk.StringVar()
+    WakeIntervalUnitsBox = ttk.Combobox(root, width = 7, textvariable = WakeIntervalUnitsVar, values = ["seconds", "minutes", "hours"])
+    WakeIntervalUnitsBox.current(0)
+    
     WakeBox.select()
-    WakeBox.grid(row = 19, column = 0, columnspan = 9)
+    WakeIntervalToggle()
     
     Text1_1 = tk.Label(root, text = "")
-    Text1_1.grid(row = 21, column = 2)
+    Text1_1.grid(row = 31, column = 2, columnspan = 6)
     Text1_2 = tk.Label(root, text = "")
-    Text1_2.grid(row = 22, column = 2)
+    Text1_2.grid(row = 32, column = 2, columnspan = 6)
     Text2_1 = tk.Label(root, text = "")
-    Text2_1.grid(row = 21, column = 3)
+    Text2_1.grid(row = 31, column = 8, columnspan = 6)
     Text2_2 = tk.Label(root, text = "")
-    Text2_2.grid(row = 22, column = 3)
+    Text2_2.grid(row = 32, column = 8, columnspan = 6)
     Text3_1 = tk.Label(root, text = "")
-    Text3_1.grid(row = 21, column = 4)
+    Text3_1.grid(row = 31, column = 14, columnspan = 6)
     Text3_2 = tk.Label(root, text = "")
-    Text3_2.grid(row = 22, column = 4)
+    Text3_2.grid(row = 32, column = 14, columnspan = 6)
     Text4_1 = tk.Label(root, text = "")
-    Text4_1.grid(row = 21, column = 5)
+    Text4_1.grid(row = 31, column = 20, columnspan = 6)
     Text4_2 = tk.Label(root, text = "")
-    Text4_2.grid(row = 22, column = 5)
+    Text4_2.grid(row = 32, column = 20, columnspan = 6)
     Text5_1 = tk.Label(root, text = "")
-    Text5_1.grid(row = 21, column = 6)
+    Text5_1.grid(row = 31, column = 26, columnspan = 6)
     Text5_2 = tk.Label(root, text = "")
-    Text5_2.grid(row = 22, column = 6)
+    Text5_2.grid(row = 32, column = 26, columnspan = 6)
     
     StartButton = tk.Button(root, text = "Start", command = Start)
-    StartButton.grid(row = 31, column = 0, columnspan = 9)
+    StartButton.grid(row = 41, column = 0, columnspan = 34)
     StopButton = tk.Button(root, text = "Stop", command = Stop)
-    StopButton.grid(row = 32, column = 0, columnspan = 9)
+    StopButton.grid(row = 42, column = 0, columnspan = 34)
     
     root.mainloop()
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # FolderUpdateChecker
-Opens a GUI where you can select up to five folders. The script checks every minute to check if the folder's contents have changed, and if they have, it beeps.
-The script can be started and stopped using the buttons.
-The script also has a "Keep Awake" option that will use the volume up and volume down keyboard buttons to keep the system awake.
+Opens a GUI where you can select up to five folders. When started it, checks on a specified time interval for if a folder's contents have changed, and can alert relative to whether a change has occurred or not.
+
+## Options:
+* Check Interval: Adjusts the time interval for when the folders are checked. Accepts an integer. Units can be changed to seconds, minutes, or hours.
+* Alert: Changes whether to beep when a folder changes, to beep when a folder doesn't change, or to not beep at all.
+* Keep Awake: Toggles whether the volume mute button will be used to keep the system awake. A double tap is used to keep the current mute state.
+  * Wake Interval: Adjusts the time interval for when the wake feature is used. Accepts an integer. Units can be changed to seconds, minutes, or hours.
